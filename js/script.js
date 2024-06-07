8a7c829299daf1faf537e7036989b23dabd604d8
Sudhir <sudhir.gunaseelan@gmail.com>
2024-06-07 17:26:30 -0400

    Updated js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ Assignment: Interactive Dynamic Table
 Sudhir Gunaseelan, UMass Lowell Computer Science, Sudhir_Gunaseelan@student.uml.edu
 Copyright (c) 2024 by Sudhir. All rights reserved. May be freely copied or
 excerpted for educational purposes with credit to the author.
-Updated by SG on June 06, 2024, at 12:00 PM.
+Updated by SG on June 07, 2024, at 6:00 PM.
 Instructor: Professor Wenjin Zhou
 Help: w3schools, Google
 Basic Description: provides the functionality for the Multiplication Table
@@ -76,7 +76,7 @@ $(document).ready(function() {
             error.insertAfter(element);
         },
         submitHandler: function(form) {
-            generateTable();
+            saveTable();
             return false;
         }
     });
@@ -97,12 +97,13 @@ $(document).ready(function() {
         var value = $(this).val();
         var sliderId = $(this).next(".slider").attr("id");
         $("#" + sliderId).slider("value", value);
+        $("#multiplicationForm").valid() && updateTable();
     });
 
     $("#tabs").tabs();
 
     // Generate multiplication table
-    function generateTable() {
+    function updateTable() {
         const startX = parseInt(document.getElementById('startX').value);
         const endX = parseInt(document.getElementById('endX').value);
         const startY = parseInt(document.getElementById('startY').value);
@@ -110,17 +111,44 @@ $(document).ready(function() {
 
         const tableHTML = createTableHTML(startX, endX, startY, endY);
 
-        tableCount++;
-        const tabId = "tab-" + tableCount;
-        const tabLabel = `Table ${tableCount}`;
+        if ($("#tabs .ui-tabs-panel").length === 0) {
+            addNewTab(tableHTML);
+        } else {
+            const activeTab = $("#tabs").tabs("option", "active");
+            const activeTabId = $("#tabs .ui-tabs-panel").eq(activeTab).attr("id");
+            $("#" + activeTabId + " .table-container").html(tableHTML);
+            updateTabLabel(activeTab, startX, endX, startY, endY);
+        }
+    }
 
-        if (tableCount === 1) {
-            $("#tabs ul li:first-child a").attr("href", `#${tabId}`).text(tabLabel);
-            $("#tabs ul li:first-child").append(`<span class='ui-icon ui-icon-close' role='presentation'></span>`);
-            $("#tab-1").attr("id", tabId).html(tableHTML);
+    // Save the table to a new tab
+    function saveTable() {
+        const startX = parseInt(document.getElementById('startX').value);
+        const endX = parseInt(document.getElementById('endX').value);
+        const startY = parseInt(document.getElementById('startY').value);
+        const endY = parseInt(document.getElementById('endY').value);
+
+        const tableHTML = createTableHTML(startX, endX, startY, endY);
+        addNewTab(tableHTML, true);
+    }
+    
+    // Add new tab when saving the table
+    function addNewTab(tableHTML, replaceInputTab = false) {
+        tableCount++;
+        const startX = parseInt(document.getElementById('startX').value);
+        const endX = parseInt(document.getElementById('endX').value);
+        const startY = parseInt(document.getElementById('startY').value);
+        const endY = parseInt(document.getElementById('endY').value);
+        const tabId = "tab-" + tableCount;
+        const tabLabel = `(${startX}-${endX}, ${startY}-${endY})`;
+
+        if (replaceInputTab && tableCount === 1) {
+            // Replace the Input tab with the first table
+            $("#tabs ul li:first-child").html(`<input type='checkbox' class='tab-checkbox' data-tab='${tabId}'><a href='#${tabId}'>${tabLabel}</a><span class='ui-icon ui-icon-close' role='presentation'></span>`);
+            $("#tabs #tab-1").attr("id", tabId).html(`<div class="table-container">${tableHTML}</div>`);
         } else {
             $("#tabs ul").append(`<li><input type='checkbox' class='tab-checkbox' data-tab='${tabId}'><a href='#${tabId}'>${tabLabel}</a><span class='ui-icon ui-icon-close' role='presentation'></span></li>`);
-            $("#tabs").append(`<div id='${tabId}'>${tableHTML}</div>`);
+            $("#tabs").append(`<div id='${tabId}' class="ui-tabs-panel"><div class="table-container">${tableHTML}</div></div>`);
         }
 
         $("#tabs").tabs("refresh");
@@ -131,6 +159,11 @@ $(document).ready(function() {
             $("#" + panelId).remove();
             $("#tabs").tabs("refresh");
         });
+    }
+
+    function updateTabLabel(index, startX, endX, startY, endY) {
+        const tabLabel = `(${startX}-${endX}, ${startY}-${endY})`;
+        $("#tabs ul li").eq(index).find("a").text(tabLabel);
     }
 
     function createTableHTML(startX, endX, startY, endY) {
@@ -165,4 +198,17 @@ $(document).ready(function() {
         });
         $("#tabs").tabs("refresh");
     });
+
+    $("#resetSliders").on("click", function() {
+        $("#startX").val(0).trigger("change");
+        $("#endX").val(0).trigger("change");
+        $("#startY").val(0).trigger("change");
+        $("#endY").val(0).trigger("change");
+        $(".slider").each(function() {
+            $(this).slider("value", 0);
+        });
+    });
+
+    // Delete enabled by default
+    $("#deleteSelected").prop("disabled", false);
 });
